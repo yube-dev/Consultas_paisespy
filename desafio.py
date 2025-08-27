370e16a6bcb3fe9a7d9059f7dab1d0c63bc62f3f
Ronald Yubero <ronald39.yubero@gmail.com>
2025-08-26 21:10:17 -0300

    segundo commit conexion

--- desafio.py
+++ desafio.py
@@ -1,7 +1,7 @@
 while True:
     pais = input("Ingrese el nombre de un pais: ").lower().strip() 
 
-    respuesta = input("Quieres consultar otro pais? (s/n): ").lower()
-
+    respuesta = input("Quieres consultar otr pais? (s/n): ").lower()
+#hola prueba
     if respuesta == "n":
         break
