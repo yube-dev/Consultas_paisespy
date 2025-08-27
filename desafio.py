00dcd85cdb116472ed80446264aa045f1a8e9630
Ronald Yubero <ronald39.yubero@gmail.com>
2025-08-27 20:34:09 -0300

    update
    Agregado API

--- desafio.py
+++ desafio.py
@@ -1,7 +1,19 @@
+import requests
+
+def comprobar_nombre(nombre_pais):
+    while nombre_pais == "":
+        nombre_pais = (input('El nombre del pais no debe estar vacio, favor reintentar: ').lower().strip())
+    return nombre_pais
+
+def consultar_pais(nombre_pais):
+    url = f"https://restcountries.com/v3.1/name/{nombre_pais}"
+
+
+
 while True:
     pais = input("Ingrese el nombre de un pais: ").lower().strip() 
+    pais = comprobar_nombre(pais)
+    respuesta = input("Quieres consultar otro pais? (s/n): ").lower()
 
-    respuesta = input("Quieres consultar otr pais? (s/n): ").lower()
-#hola prueba
     if respuesta == "n":
         break
