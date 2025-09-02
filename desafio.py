7ff262ef3feed4b8109ff41d46cd359abad7cc1e
ryube <ronald39.yubero@gmail.com>
2025-09-01 21:02:00 -0300

    Update
    falta guardado, el resto de las consultas ya estarían a un 70%

--- desafio.py
+++ desafio.py
@@ -16,19 +16,29 @@ def consultar_pais(nombre_pais):
     respuesta = requests.get(url)
     if respuesta.status_code == 200:
         datos = respuesta.json()
-        print(datos[0].keys())
+        info_pais = {
+            "nombre": datos[0]["name"]["official"],
+            "capital": datos[0]["capital"],
+            "region": datos[0]["region"],
+            "poblacion": datos[0]["population"],
+            "idiomas": list(datos[0]["languages"].values()),
+        }
+        print(info_pais)
+        return info_pais
+
     elif respuesta.status_code == 404:
         print(f"Pais no encontrado {nombre_pais} no existe")
+    return None
 
 
 while True:
     pais = input("Ingrese el nombre de un pais: ").lower().strip()
     pais = comprobar_nombre(pais)
 
-    consultar_pais(pais)
+    informacion = consultar_pais(pais)
 
     respuesta = input("Quieres consultar otro pais? (s/n): ").lower()
 
     if respuesta == "n":
         break
-# video 46:12, explicacion de la estructura de datos
+# video 1:09:33 , explicacion de la estructura de datos
